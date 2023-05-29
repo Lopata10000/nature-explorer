6c0764a463e42a3ee8e6f167e04b549d3c8df945
Фанта Петро <c.fanta.petro@student.uzhnu.edu.ua>
2023-05-29 21:23:50 +0300

    Initial commit

---
+++ src/main/java/com/fanta/natureexplorer/NatureExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.fanta.natureexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatureExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NatureExplorerApplication.class, args);
+    }
+
+}
